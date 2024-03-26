382aa79486636cd9cd183151fce05fdb26c1fe42
DrParadox7 <gustavo.caliari@live.com>
2024-03-26 18:35:50 +0100

    Disable AROMA [until cave height is increased]
    Disable mixins responsible for increasing world height.
    To be reverted when Immersive cavegen mixins are able to increase its cave gen height to be above 120Y

--- src/main/java/sexy/poke/mixinplugin/Mixins.java
+++ src/main/java/sexy/poke/mixinplugin/Mixins.java
@@ -17,7 +17,7 @@ public enum Mixins {
                     "aroma.MixinBiomeGenMining",
                     "aroma.MixinChunkProviderMining",
                     "aroma.MixinWorldProviderMiner")
-            .setApplyIf(() -> true).addTargetedMod(TargetedMod.AROMA)),
+            .setApplyIf(() -> false).addTargetedMod(TargetedMod.AROMA)),
 
     GOG3MIXIN(new Builder("GoG3Mixin").setPhase(Phase.LATE).setSide(Side.BOTH)
             .addMixinClasses(
